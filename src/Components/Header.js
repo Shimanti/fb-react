2f534a618249200df40b98514ddb526a093f2bfb
Shimanti <shimanti@gmail.com>
2021-08-01 17:19:43 -0700

    package info

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header__left'>
+        <img src="https://upload.wikimedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png" alt="Facebook logo" />
+    </div>
+  )
+}
